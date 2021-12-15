421bc3d2cb250c8126271d6fdca2c19f2ba0157f
tinyhouse-entrance <96177804+tinyhouse-entrance@users.noreply.github.com>
2021-12-15 12:02:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
 
-@app.route('/')
+# A simple Flask App which takes
+# a user's name as input and responds
+# with "Hello {name}!"
 
-def hello():
-    return("Hello world")
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = ''
+    if flask.request.method == 'POST':
+        message = 'Hello ' + flask.request.form['name-input'] + '!'
+    return flask.render_template('index.html', message=message)
 
 if __name__ == '__main__':
     app.run()
