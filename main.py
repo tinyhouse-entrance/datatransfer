e720a4285ce1f33b3b80d81bedb0587df82d589a
tinyhouse-entrance <96177804+tinyhouse-entrance@users.noreply.github.com>
2021-12-15 12:38:43 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,88 @@
-import flask
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+app = Flask(__name__)
+api = Api(app)
 
-# A simple Flask App which takes
-# a user's name as input and responds
-# with "Hello {name}!"
+class Users(Resource):
+    def get(self):
+        data = pd.read_csv('users.csv')  # read CSV
+        data = data.to_dict()  # convert dataframe to dictionary
+        print(data)
+        return {'data': data}, 200  # return data and 200 OK code
+    
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
+        
+        args = parser.parse_args()  # parse arguments to dictionary
+        
+        # create new dataframe containing new values
+        new_data = pd.DataFrame({
+            'userId': args['userId'],
+            'name': args['name'],
+            'city': args['city'],
+            'locations': args['locations']
+        })
+        # read our CSV
+        data = pd.read_csv('users.csv')
+        
+        if args['userId'] in list(data['userId']):
+            return {
+                'message': f"'{args['userId']}' already exists."
+            }, 401
+        else:
+            # create new dataframe containing new values
+            new_data = pd.DataFrame({
+                'userId': args['userId'],
+                'name': args['name'],
+                'city': args['city'],
+                'locations': args['locations']
+            })
 
-app = flask.Flask(__name__)
+        # add the newly provided values
+        data = data.append(new_data, ignore_index=True)
+        # save back to CSV
+        data.to_csv('users.csv', index=False)
+        return {'data': data.to_dict()}, 200  # return data with 200 OK
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    message = ''
-    if flask.request.method == 'POST':
-        message = 'Hello ' + flask.request.form['name-input'] + '!'
-    return flask.render_template('index.html', message=message)
+    def put(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('users.csv')
+        
+        if args['userId'] in list(data['userId']):
+            # evaluate strings of lists to lists
+            data['locations'] = data['locations'].apply(
+                lambda x: ast.literal_eval(x)
+            )
+            # select our user
+            user_data = data[data['userId'] == args['userId']]
+
+            # update user's locations
+            user_data['locations'] = user_data['locations'].values[0] \
+                .append(args['location'])
+            
+            # save back to CSV
+            data.to_csv('users.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+
+        else:
+            # otherwise the userId does not exist
+            return {
+                'message': f"'{args['userId']}' user not found."
+            }, 404
+
+api.add_resource(Users, '/users')  # '/users' is our entry point
 
 if __name__ == '__main__':
-    app.run()
+    app.run()  # run our Flask app
