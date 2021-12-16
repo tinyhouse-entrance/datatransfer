2bd7486f0ab73a38cfd8198e3e9cf23cca624c7b
tinyhouse-entrance <96177804+tinyhouse-entrance@users.noreply.github.com>
2021-12-16 17:19:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ api = Api(app)
 app.route('/')
 #endregion variables
 
+
 class Data(Resource):
 
     def post(self):
@@ -38,36 +39,33 @@ class Data(Resource):
         # add the newly provided values
         data = data.append(new_data, ignore_index=True)
         # save back to CSV
-        data.to_csv('data.csv', index=False)
+        #data.to_csv('data.csv', index=False)
         return {'data': data.to_dict()}, 200  # return data with 200 OK
         
     def put(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('userId', required=True)  # add args
-        parser.add_argument('locations', required=True)
+        parser = reqparse.RequestParser()  # initialize     
+        parser.add_argument('timestamp', required=True)  # add args
+        parser.add_argument('t1', required=True)
+        parser.add_argument('t2', required=True)
+        parser.add_argument('t3', required=True)
+        parser.add_argument('t4', required=True)
+        parser.add_argument('co2', required=True)      
         args = parser.parse_args()  # parse arguments to dictionary
 
         # read our CSV
         data = pd.read_csv('data.csv')
-        
-        if args['userId'] in list(data['userId']):
-            # evaluate strings of lists to lists
-            # select our user
-            user_data = data[data['userId'] == args['userId']]
-
-            # update user's locations
-            user_data['locations'] = args['locations']
-            print(args['locations'])
+            
+        # update data
+        data['timestamp'] = args['timestamp']
+        data['t1'] = args['t1']
+        data['t2'] = args['t2']
+        data['t3'] = args['t3']
+        data['t4'] = args['t4']
+        data['co2'] = args['co2']
             # save back to CSV
-            data.to_csv('data.csv', index=False)
+        data.to_csv('data.csv', index=False)
             # return data and 200 OK
-            return {'data': data.to_dict()}, 200
-
-        else:
-            # otherwise the userId does not exist
-            return {
-                'message': f"'{args['userId']}' user not found."
-            }, 404
+        return {'data': data.to_dict()}, 200
 
     def get(self):
         data = pd.read_csv('data.csv')  # read CSV
@@ -75,8 +73,43 @@ class Data(Resource):
         #print(data)
         return {'data': data}, 200  # return data and 200 OK code
 
+class TinyHouseData(Resource):
+    def put(self):
+        parser = reqparse.RequestParser()  # initialize     
+        parser.add_argument('timestamp', required=True)  # add args
+        parser.add_argument('t1', required=True)
+        parser.add_argument('t2', required=True)
+        parser.add_argument('t3', required=True)
+        parser.add_argument('t4', required=True)
+        parser.add_argument('co2', required=True)      
+        args = parser.parse_args()  # parse arguments to dictionary
 
-api.add_resource(Data, '/data')  # '/data' is our entry point
+        # read our CSV
+        data = pd.read_csv('tinydata.csv')
+            
+        # update data
+        data['timestamp'] = args['timestamp']
+        data['t1'] = args['t1']
+        data['t2'] = args['t2']
+        data['t3'] = args['t3']
+        data['t4'] = args['t4']
+        data['co2'] = args['co2']
+            # save back to CSV
+        data.to_csv('tinydata.csv', index=False)
+            # return data and 200 OK
+        return {'data': data.to_dict()}, 200
+
+    def get(self):
+        data = pd.read_csv('tinydata.csv')  # read CSV
+        data = data.to_dict()  # convert dataframe to dictionary
+        #print(data)
+        return {'data': data}, 200  # return data and 200 OK code
+
+
+
+api.add_resource(Data, '/data')  #: '/data' is our entry point
+api.add_resource(TinyHouseData,'/tinyhousedata')
+
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
